4230112eac369ba5a76f90ca0835fd5f0d34880d
dim321 <tunturidim@gmail.com>
2026-08-13 20:09:38 +0700

    integration tests added

---
+++ spec/support/advertising_network.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+module AdvertisingNetwork
+  WEEKLY_HOURS = Location::OperatingHours::DAY_KEYS.index_with do
+    [ { 'start' => '09:00', 'end' => '21:00' } ]
+  end.freeze
+
+  def create_advertising_network!
+    operator = create(:organization, :operator, name: 'Advertising company')
+    create(
+      :user,
+      :administrator,
+      organization: operator,
+      email: 'admin@advertising.test'
+    )
+
+    komandor = create(:organization, :client, name: 'Командор')
+    alleya = create(:organization, :client, name: 'Аллея')
+    create(:user, :manager, organization: komandor, email: 'manager@komandor.test')
+    alleya_manager = create(:user, :manager, organization: alleya, email: 'manager@alleya.test')
+
+    locations = [ 'Локация 1', 'Локация 2', 'Локация 3' ].map do |name|
+      create(:location, name: name, operating_hours: WEEKLY_HOURS)
+    end
+    locations.each do |location|
+      create(:station, location: location, name: 'Станция A')
+      create(:station, location: location, name: 'Станция B')
+    end
+
+    station = locations.first.stations.find_by!(name: 'Станция A')
+    screens = [ 'Витрина 1', 'Витрина 2', 'Витрина 3' ].map do |name|
+      create(:screen, name: name, station: station, owner_organization: komandor)
+    end
+
+    group = create(
+      :broadcast_point_group,
+      organization: komandor,
+      name: 'Витрины Командор',
+      commercial_quota_percent: 60,
+      commercial_quota_period: :hour
+    )
+    screens.each do |screen|
+      create(:broadcast_point_group_membership, broadcast_point_group: group, screen: screen)
+    end
+
+    {
+      operator: operator,
+      komandor: komandor,
+      alleya: alleya,
+      alleya_manager: alleya_manager,
+      group: group,
+      screens: screens
+    }
+  end
+end
+
+RSpec.configure do |config|
+  config.include AdvertisingNetwork, type: :system
+end
