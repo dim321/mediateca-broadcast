674fd38dde959b6c7a92b2ee8aad92f8cdbc977c
dim321 <tunturidim@gmail.com>
2026-09-17 13:12:30 +0700

    fix rubocop

--- spec/requests/admin/broadcast_portraits_spec.rb
+++ spec/requests/admin/broadcast_portraits_spec.rb
@@ -24,6 +24,27 @@ RSpec.describe "Admin broadcast portraits", type: :request do
     }.merge(overrides)
   end
 
+  def patch_portrait_with_welcome(portrait, theme, time_of_day:)
+    patch admin_broadcast_portrait_path(portrait), params: {
+      broadcast_portrait: {
+        name: portrait.name,
+        block_frequencies_per_hour: portrait.block_frequencies_per_hour,
+        max_commercial_in_row: portrait.max_commercial_in_row,
+        neutral_min_seconds: portrait.neutral_min_seconds,
+        blocks: [
+          { position: 1, kind: "commercial" },
+          {
+            position: 2,
+            kind: "service_welcome",
+            service_theme_id: theme.id,
+            pick_strategy: "sequential",
+            time_of_day: time_of_day
+          }
+        ]
+      }
+    }
+  end
+
   describe "authentication" do
     it "redirects guests to login" do
       get admin_broadcast_portraits_path
@@ -201,47 +222,11 @@ RSpec.describe "Admin broadcast portraits", type: :request do
       theme = create(:service_theme, organization: operator_org)
       create(:broadcast_portrait_block, :commercial, broadcast_portrait: portrait, position: 1)
 
-      patch admin_broadcast_portrait_path(portrait), params: {
-        broadcast_portrait: {
-          name: portrait.name,
-          block_frequencies_per_hour: portrait.block_frequencies_per_hour,
-          max_commercial_in_row: portrait.max_commercial_in_row,
-          neutral_min_seconds: portrait.neutral_min_seconds,
-          blocks: [
-            { position: 1, kind: "commercial" },
-            {
-              position: 2,
-              kind: "service_welcome",
-              service_theme_id: theme.id,
-              pick_strategy: "sequential",
-              time_of_day: "12:30"
-            }
-          ]
-        }
-      }
-
+      patch_portrait_with_welcome(portrait, theme, time_of_day: "12:30")
       welcome = portrait.reload.blocks.find_by!(kind: "service_welcome")
       expect(welcome.time_of_day.strftime("%H:%M")).to eq("12:30")
 
-      patch admin_broadcast_portrait_path(portrait), params: {
-        broadcast_portrait: {
-          name: portrait.name,
-          block_frequencies_per_hour: portrait.block_frequencies_per_hour,
-          max_commercial_in_row: portrait.max_commercial_in_row,
-          neutral_min_seconds: portrait.neutral_min_seconds,
-          blocks: [
-            { position: 1, kind: "commercial" },
-            {
-              position: 2,
-              kind: "service_welcome",
-              service_theme_id: theme.id,
-              pick_strategy: "sequential",
-              time_of_day: ""
-            }
-          ]
-        }
-      }
-
+      patch_portrait_with_welcome(portrait, theme, time_of_day: "")
       expect(portrait.reload.blocks.find_by!(kind: "service_welcome").time_of_day).to be_nil
     end
 
